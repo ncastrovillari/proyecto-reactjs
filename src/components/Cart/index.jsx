1edc8d4adf307f3ccb635509e93b497eeacce517
nicolas castrovillari <ncastrovillari@gmail.com>
2022-10-13 01:45:11 -0300

    desafio clase 13 Firebase II

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,9 +1,32 @@
 import { VStack, Center, Text, Image, HStack, Button } from "@chakra-ui/react"
 import { useCartContext } from "../../context/CartContext"
+import { db } from "../../firebase/firebase"
+import { collection, addDoc, serverTimestamp } from "firebase/firestore"
 
 const Cart = () => {
 
     const { cartList, totalPrice, removeProduct, cleanCart } = useCartContext()
+
+    const comprador = {
+        nombre: "Juan",
+        apellido: "Perez",
+        email: "juanperez@gmail.com",
+        telefono: "1167891234"
+    };
+
+    const finalizarCompra = ()=>{
+        const ventasCollection = collection(db, "ventas")
+        addDoc(ventasCollection, {
+            comprador,
+            items: cartList,
+            date: serverTimestamp(),
+            totalPrice: totalPrice(),
+        })
+        .then(result=>{
+            console.log(result.id);
+            cleanCart();
+        })
+    }
 
     return (
         <Center>
@@ -23,6 +46,7 @@ const Cart = () => {
                     <>
                     <Text as="b">Total: ${totalPrice()}</Text>
                     <Button variant="solid" colorScheme="green" size="sm" onClick={cleanCart}>Vaciar carrito</Button>
+                    <Button variant="solid" colorScheme="green" size="sm" onClick={finalizarCompra}>Finalizar compra</Button>
                     </>
                 }
             </VStack>
