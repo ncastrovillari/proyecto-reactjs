cac8ca97b5862964bb9962c02f9a9c86656ba62d
nicolas castrovillari <ncastrovillari@gmail.com>
2022-10-14 00:36:51 -0300

    stock

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,11 +1,14 @@
 import { VStack, Center, Text, Image, HStack, Button } from "@chakra-ui/react"
 import { useCartContext } from "../../context/CartContext"
 import { db } from "../../firebase/firebase"
-import { collection, addDoc, serverTimestamp } from "firebase/firestore"
+import { collection, addDoc, serverTimestamp, doc, updateDoc } from "firebase/firestore"
+import { useState } from "react"
+import { products } from "../../utils/products"
 
 const Cart = () => {
 
     const { cartList, totalPrice, removeProduct, cleanCart } = useCartContext()
+    const { idCompra, setIdCompra } = useState("")
 
     const comprador = {
         nombre: "Juan",
@@ -22,10 +25,23 @@ const Cart = () => {
             date: serverTimestamp(),
             totalPrice: totalPrice(),
         })
-        .then(result=>{
+        .then((result) => {
             console.log(result.id);
-            cleanCart();
+            setIdCompra(result.id)
+            cartList.forEach(product => {
+                actualizarStock(product)
+            });
         })
+        setTimeout(() => {
+            cleanCart();
+        }, 5000);    
+        
+    }
+
+    const actualizarStock = (product) => {
+        const updateStock = doc(db, "productos", products.id)
+        updateDoc(updateStock, {stock: (products.stock - products.quantity)})
+        console.log(products.stock);
     }
 
     return (
@@ -46,7 +62,7 @@ const Cart = () => {
                     <>
                     <Text as="b">Total: ${totalPrice()}</Text>
                     <Button variant="solid" colorScheme="green" size="sm" onClick={cleanCart}>Vaciar carrito</Button>
-                    <Button variant="solid" colorScheme="green" size="sm" onClick={finalizarCompra}>Finalizar compra</Button>
+                    <Button variant="solid" colorScheme="green" size="sm" onClick={finalizarCompra} idCompra={idCompra}>Finalizar compra</Button>
                     </>
                 }
             </VStack>
