09a91b144df784f348785a818aa0c62f53979cff
nicolas castrovillari <ncastrovillari@gmail.com>
2022-11-11 00:45:10 -0300

    carrito vacio

--- src/components/cartWidget/index.jsx
+++ src/components/cartWidget/index.jsx
@@ -3,10 +3,12 @@ import { Text } from '@chakra-ui/react'
 import { NavLink } from "react-router-dom";
 import { useCartContext } from "../../context/CartContext"
 
-
 const CartWidget = () => {
 
-    const { totalQuantity } = useCartContext()
+    const { totalQuantity, cartList } = useCartContext()
+    if(cartList.length === 0) {
+        return ('')
+        }
 
     return(
         <NavLink to="/cart">
